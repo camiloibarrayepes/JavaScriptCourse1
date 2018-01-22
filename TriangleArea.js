95bfd108d01bb77c1dc561af4c4e6699341f1600
camilo andres ibarra yepes <camilo.ibarray@gmail.com>
2018-01-21 21:11:11 -0500

    TriangleArea Example

---
+++ TriangleArea.js
@@ -0,0 +1,24 @@
+let base=5
+let height = 7
+
+function trianguloArea(base, height){
+	return base * height/2
+}
+
+console.log(`el area del triangulo de base ${base} y altura ${height} es: ${trianguloArea(base,height)}`)
+
+//Segunda forma de escribir este código
+
+/*
+let trianguloArea = (base, height){
+	return base * height/2
+}
+*/
+
+//Tercera forma de escribir este código
+
+/*
+let trianguloArea = (base, height) => base * height/2
+
+*/
+
